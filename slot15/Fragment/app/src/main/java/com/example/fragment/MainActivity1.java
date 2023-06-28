fed952b943577ac1347175a2fe50ea4c98fb210b
toumtam2100 <toumtampde160576@fpt.edu.vn>
2023-06-28 21:52:30 +0700

    slot15

---
+++ slot15/Fragment/app/src/main/java/com/example/fragment/MainActivity1.java
@@ -0,0 +1,21 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.your_placeholder1, new fragmentOne());
+        ft.replace(R.id.your_placeholder2, new fragmentTwo());
+        // Complete the changes added above
+        ft.commit();
+    }
+}
